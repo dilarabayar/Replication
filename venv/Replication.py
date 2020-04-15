2d1813de17c52b6e861bd74691b487f526fcf192
Dilara Bayar <dilarabayar@marun.edu.tr>
2020-04-15 10:35:19 +0300

    Reverse Complementsiz son hali

--- venv/Replication.py
+++ venv/Replication.py
@@ -1,62 +1,47 @@
-
-def bruteForce(text,pattern):
-
-    count = 0
-
-    if pattern in text:
-        ind = text.find(pattern)
-        while ind!=-1:
-            ind = text.find(pattern,ind+1)
-            count = count + 1
-#eğer patterndan sadece 1 tane vaarsa kmers olmuyor o yüzden eklemesin.
-    if (count > 1):
-        kmers.append(pattern)
-        print(pattern)
-        rates.append(count)
-    pass
-
 def search(text):
-    i = 0
+    gecici = dict()
+    kmers = dict()
+    kmer_list = []
 
-#Inner for loop doğru çalışıyor ancak outer ekleyince çok uzun sürüyor ve ikinci for loopa sadece 1 kez giriyor.
     for j in range(5,len(text)):
-        for pattern in range(0, len(text)):
+
+        for i in range(0, len(text)):
             pattern = text[i:i+j]
-            #if statement sadece boyutu (ör:başta 5 olanları) alsın ve kmers'de yoksa brute force yapsın dedim.aynı string için tekrar tekrar arama yapmasın diye.
-            if ((len(pattern) == j) and (pattern not in kmers)):
-                bruteForce(text,pattern)
-            i = i + 1
-        print(j)
-        j = j + 1
-pass
+            if (len(pattern) != j):
+                break
+
+            if ((pattern in gecici.keys()) and (pattern not in kmers.keys())):
+                kmers[pattern] = 2  # Geçiciye eklerken de bir tane vardı o yüzden burası 2'den başlayacak
+            elif (pattern in kmers.keys()):
+                kmers[pattern] += 1  #Artık bundan 1'den fazla olduğu için bu string için geçiciyle işimiz yok
+            else:
+                gecici[pattern] = 1
+
+        if len(kmers) < 1:
+            break  # Bu k ile hiç tekrarlayan string bulunmadı o yüzden daha ileriye gitmeye gerek yok
+        kmer_list.append(kmers.copy())
+        gecici.clear()
+        kmers.clear()  # Bunu da boşalttık çünkü bize en uzun k-merler lazım
+
+    return kmer_list[len(kmer_list) - 1]  # Son elemanı döndür
 
 
 def main():
-
-    global rates, kmers
-#kmers pattern tutmak için rates de kaç defa stringde bulunduklarını tutmak için. nepal ve wuhan için ayrı ayrı array gerekiyor.
-    rates = []
-    kmers = []
-
-    '''Texti bütün tek bir string haline getiriyor.
-        Method olarak yapacaktım aslında ama open içine variable yazmak sürekli hata verdiği için beceremedim LOL o yüzden her text için
-        ayrı ayrı open methodları var.'''
 
     with open("wuhan.txt", 'r') as file:
         wuhan = file.read().replace('\n', '').replace(' ', '')
         wuhan = ''.join([i for i in wuhan if not i.isdigit()])
 
+    longest_kmers = search(wuhan)
+    print("wuhan", str(longest_kmers))
+
     with open("nepal.txt", 'r') as file:
         nepal = file.read().replace('\n', '').replace(' ', '')
         nepal = ''.join([i for i in nepal if not i.isdigit()])
 
-    search(nepal)
+    longest_kmers = search(nepal)
+    print("nepal", str(longest_kmers))
 
-    for x in kmers:
-        print(x)
-    for y in rates:
-        print(y)
-    pass
 
 if __name__ == "__main__":
     main()
